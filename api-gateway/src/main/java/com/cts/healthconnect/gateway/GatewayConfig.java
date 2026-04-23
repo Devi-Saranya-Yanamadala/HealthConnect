21db16155f69a2da7c61fbd3751404523e90268d
Devi Saranya <2484926@cognizant.com>
2026-04-23 18:05:33 +0530

    Added API Gateway

---
+++ api-gateway/src/main/java/com/cts/healthconnect/gateway/GatewayConfig.java
@@ -0,0 +1,48 @@
+package com.cts.healthconnect.gateway;
+
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+            .route("auth", r -> r.path("/api/auth/**")
+                .uri("http://localhost:5006"))
+
+            .route("doctor", r -> r.path("/api/doctors/**")
+                .uri("http://localhost:5000"))
+
+            .route("slot", r -> r.path("/api/slots/**")
+                .uri("http://localhost:5001"))
+
+            .route("appointment", r -> r.path("/api/appointments/**")
+                .uri("http://localhost:5002"))
+
+            .route("patient", r -> r.path("/api/patients/**")
+                .uri("http://localhost:5003"))
+
+            .route("notification", r -> r.path("/api/notifications/**")
+                .uri("http://localhost:5004"))
+
+            .route("invoice", r -> r.path("/api/invoices/**")
+                .uri("http://localhost:5005"))
+
+            .route("claim", r -> r.path("/api/claims/**")
+                .uri("http://localhost:5005"))
+
+            .route("ward", r -> r.path("/api/wards/**")
+                .uri("http://localhost:5007"))
+
+            .route("analytics", r -> r.path("/api/analytics/**")
+                .uri("http://localhost:5008"))
+
+            .build();
+    }
+}
---
+++ api-gateway/src/test/java/com/cts/healthconnect/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.healthconnect.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

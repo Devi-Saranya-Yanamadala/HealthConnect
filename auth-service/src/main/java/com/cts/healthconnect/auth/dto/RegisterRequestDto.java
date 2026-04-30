4536828e773babb0f3c8f407fcc3dcfdeecb0138
Devi-Saranya-Yanamadala <Yanamadala.DeviSaranya@cognizant.com>
2026-04-30 11:37:21 +0530

    Added imports for email and pattern

--- auth-service/src/main/java/com/cts/healthconnect/auth/dto/RegisterRequestDto.java
+++ auth-service/src/main/java/com/cts/healthconnect/auth/dto/RegisterRequestDto.java
@@ -4,6 +4,8 @@ import com.cts.healthconnect.auth.entity.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
